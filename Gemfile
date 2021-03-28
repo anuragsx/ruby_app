0c2d1f9e3f9344e525b71090dfb1cc7db3d3c2ce
Anurag Saxena <karisaxena@gmail.com>
2021-03-28 18:30:07 +0100

    smart pension test code changes

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+group :development, :test do
+  # gem "rails"
+  gem 'rspec', '~> 3.8'
+end
---
+++ lib/log_analyzer.rb
@@ -0,0 +1,39 @@
+class LogAnalyzer
+
+  def initialize(file_path)
+    raise "No such file exists at #{file_path}" unless File.exists? file_path
+    @file = File.open(file_path)
+    @log_stats = Hash.new { |h, k| h[k] = [] }
+  end
+
+  def parsing
+    @file.each do |line|
+      path, ip = line.split(' ')
+      @log_stats[path] << ip
+    end
+    @log_stats
+  end
+
+  def most_views
+    counter
+    @counts.sort_by { |_k, v| v }.reverse.to_h
+  end
+
+  def unique_views
+    @log_stats.each do |k, _v|
+      @log_stats[k].uniq!
+    end
+    counter
+    @counts.sort_by { |_k, v| v }.reverse.to_h
+  end
+
+  private
+
+  def counter
+    @counts = {}
+    @log_stats.each do |k, _v|
+      @counts[k] = @log_stats[k].length
+    end
+    @counts
+  end
+end
